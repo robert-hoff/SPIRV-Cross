dee4ede68fb7c62c1f30df7b95cc2053959aa499
Hoff Inc <hoff.rj@gmail.com>
2022-04-07 13:38:32 +0100

    working example adding two ints

--- SpirvCrossDll/spirv_cross_dll_api.h
+++ SpirvCrossDll/spirv_cross_dll_api.h
@@ -9,6 +9,12 @@ public:
 	int adding(int y);
 
 };
-
-
+extern "C" __declspec(dllexport) void *Create(int x)
+{
+	return (void *)new Attempt(x);
+}
+extern "C" __declspec(dllexport) int AttemptAdd(Attempt * a, int y)
+{
+	return a->adding(y);
+}
 
