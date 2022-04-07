dc8919d1c2938240e084acc7eecd0d82e930efc3
Hoff Inc <hoff.rj@gmail.com>
2022-04-07 05:41:02 +0100

    ran cmake .

---
+++ CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: X:/checkouts/SPIRV-Cross
+# Build directory: X:/checkouts/SPIRV-Cross
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ gitversion.h
@@ -0,0 +1,9 @@
+// Copyright 2016-2021 The Khronos Group Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef SPIRV_CROSS_GIT_VERSION_H_
+#define SPIRV_CROSS_GIT_VERSION_H_
+
+#define SPIRV_CROSS_GIT_REVISION "Git commit: 0d4ce028 Timestamp: 2022-04-07T05:39:11"
+
+#endif
